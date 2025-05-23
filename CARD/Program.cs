4dc53abf3b23fa5079173b79ec9f43fc66034539
PezzellaF <federico.pezzella@itiscuneo.eu>
2025-05-23 10:29:06 +0200

    Aggiunta models con collegamento a program.cs e lieve modifica appsettings.json

--- CARD/Program.cs
+++ CARD/Program.cs
@@ -1,15 +1,21 @@
+﻿using CardCollectionApp.Models;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// 👉 Aggiungi il DbContext con la connessione SQL Server
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+// Servizi MVC
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+// Pipeline HTTP
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
@@ -20,6 +26,7 @@ app.UseRouting();
 
 app.UseAuthorization();
 
+// 👉 Puoi cambiare Home in PokemonCards o altro se vuoi una pagina iniziale diversa
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
