c2b14b8e0a4a3dc70bf2e91ecdbcb6dee97512b3
Filippo Viano <filippo.viano@itiscuneo.eu>
2025-05-25 23:04:52 +0200

    creazione DB modifica appsettings.json

---
+++ CARD/Migrations/20250525205455_InitialCreate.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CARD.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "MagicCards",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Nome = table.Column<string>(type: "TEXT", maxLength: 50, nullable: false),
+                    Espansione = table.Column<string>(type: "TEXT", maxLength: 50, nullable: false),
+                    NumeroSerie = table.Column<string>(type: "TEXT", nullable: false),
+                    Valore = table.Column<decimal>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_MagicCards", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "PokemonCards",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Nome = table.Column<string>(type: "TEXT", maxLength: 40, nullable: false),
+                    Espansione = table.Column<string>(type: "TEXT", maxLength: 50, nullable: false),
+                    NumeroSerie = table.Column<string>(type: "TEXT", nullable: false),
+                    Valore = table.Column<decimal>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PokemonCards", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "YuGiOhCards",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Nome = table.Column<string>(type: "TEXT", maxLength: 50, nullable: false),
+                    Espansione = table.Column<string>(type: "TEXT", maxLength: 50, nullable: false),
+                    NumeroSerie = table.Column<string>(type: "TEXT", nullable: false),
+                    Valore = table.Column<decimal>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_YuGiOhCards", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "MagicCards");
+
+            migrationBuilder.DropTable(
+                name: "PokemonCards");
+
+            migrationBuilder.DropTable(
+                name: "YuGiOhCards");
+        }
+    }
+}
